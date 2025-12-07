2a8ac83a65325f71d79e27779d1c5c4c58b43ca7
Devin Brite <devin@dwbrite.com>
2025-12-07 01:25:04 -0500

    wip

---
+++ sdk-template/src/asm/example.asm
@@ -0,0 +1,11 @@
+; Example assembly function
+; This demonstrates how to write custom assembly code for your game
+
+.section .text
+.global my_custom_function
+
+my_custom_function:
+    ; Your custom assembly code here
+    ; This is just an example that does nothing
+    NOP
+    RTS
